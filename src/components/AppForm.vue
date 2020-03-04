cebc2586a109de656804474507fca858935f0e15
Prashant Shrestha <sthprashant@gmail.com>
2020-03-05 00:02:54 +1100

    few changes wit the position

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -3,13 +3,7 @@
     <v-col cols="12" lg="4" md="6" sm="8">
       <v-row justify="center">
         <v-col cols="12" lg="3" md="5" sm="7" xs="2">
-          <v-text-field
-            placeholder="LAN ID"
-            outlined
-            dense
-            v-model="lanId"
-            @input="sendLanId"
-          ></v-text-field>
+          <v-text-field placeholder="LAN ID" outlined dense v-model="lanId" @input="sendLanId"></v-text-field>
         </v-col>
       </v-row>
       <v-overflow-btn
@@ -25,21 +19,23 @@
         rounded
         height-details="300px"
       >
-      
         <template v-slot:selection="{ item, index }">
           <v-chip v-if="index === 0">
             <span>{{ item.name }}</span>
           </v-chip>
-          <span v-if="index === 1" class="grey--text caption"
-            >(+{{ selectedApps.length - 1 }} others)</span
-          >
+          <span
+            v-if="index === 1"
+            class="grey--text caption"
+          >(+{{ selectedApps.length - 1 }} others)</span>
         </template>
       </v-overflow-btn>
+
       <v-row justify="center">
-        <v-col>
+        <v-col class="text-center" cols="12" sm="4" md="4">
           <v-checkbox v-model="itSupport" label="IT Support" @change="sendCheckBoxStatus"></v-checkbox>
         </v-col>
       </v-row>
+
       <v-row justify="center">
         <v-col class="text-center" cols="12" sm="4">
           <div class="my-2">
@@ -55,13 +51,11 @@
 export default {
   name: "app-form",
   props: {
-    apps: Array,
-    
+    apps: Array
   },
   methods: {
     sendSelectedApps() {
-      if(this.lanId !== '')
-      this.$emit("send:apps", this.selectedApps);
+      if (this.lanId !== "") this.$emit("send:apps", this.selectedApps);
     },
     sendLanId() {
       this.$emit("send:lanId", this.lanId);
@@ -69,7 +63,6 @@ export default {
     },
     sendCheckBoxStatus() {
       this.$emit("send:itSupport", this.itSupport);
-      
     },
     clearData() {
       this.lanId = "";
@@ -82,7 +75,7 @@ export default {
     return {
       selectedApps: [],
       lanId: "",
-      itSupport:false,
+      itSupport: false
     };
   }
 };
